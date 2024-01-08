e3a7517e4b464c672eab2c50f4ce9f7a4beb44f8
ahmedgoweda <ahmedgoweda94@gmail.com>
2024-01-08 19:12:53 +0200

    Modify some features and tools

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
  
-var ProductNameInput = document.getElementById('ProductName');
-var ProductPriceInput = document.getElementById('ProductPrice');
-var ProductCategoryInput = document.getElementById('ProductCategory');
-var ProductDescInput = document.getElementById('ProductDesc');
-var searchInput= document.getElementById('searchInput');
-var addbtn=document.getElementById("addBtn");
-var updatebtn=document.getElementById("updtateBtn");
-var indexupdate=0;
-var addProductContainer = [];
+let ProductNameInput = document.getElementById('ProductName');
+let ProductPriceInput = document.getElementById('ProductPrice');
+let ProductCategoryInput = document.getElementById('ProductCategory');
+let ProductDescInput = document.getElementById('ProductDesc');
+let searchInput= document.getElementById('searchInput');
+let addbtn=document.getElementById("addBtn");
+let updatebtn=document.getElementById("updtateBtn");
+let indexupdate=0;
+let addProductContainer = [];
 if (localStorage.getItem("product") != null)
 {
     addProductContainer=JSON.parse(localStorage.getItem("product"))
     displayData();
 }
 function addProduct() {
-   var  Product = {
-        name: ProductNameInput.value,
-        price: ProductPriceInput.value,
-        Category: ProductCategoryInput.value,
-        Desc: ProductDescInput.value,
-    } 
-    addProductContainer.push(Product);
-localStorage.setItem("product",JSON.stringify(addProductContainer));
- displayData();
- clearform();
-
-}
+    if(validationName()==true && validationPrice()==true){
+        let  Product = {
+            name: ProductNameInput.value,
+            price: ProductPriceInput.value,
+            Category: ProductCategoryInput.value,
+            Desc: ProductDescInput.value,
+        } 
+        addProductContainer.push(Product);
+    localStorage.setItem("product",JSON.stringify(addProductContainer));
+     displayData();
+     clearform();
+    
+    }
+    }
+  
  
 function clearform(){ 
  ProductNameInput.value="";
@@ -36,8 +39,8 @@ ProductDescInput.value="";
 
 function displayData(){
 
-    var boxProduct="";
-    for(var i=0;i<addProductContainer.length ;i++){
+    let boxProduct="";
+    for(let i=0;i<addProductContainer.length ;i++){
         boxProduct +=`<tr>
         <td class="bg-dark text-white"> ${addProductContainer[i].name}</td>
         <td class="bg-dark text-white"> ${addProductContainer[i].price}</td>
@@ -57,9 +60,9 @@ function deleteProduct(ProductIndex){
 }
  
 function searchBroduct(){
-var term =searchInput.value;
-    var boxProduct="";
-    for(var i=0;i<addProductContainer.length ;i++){
+let term =searchInput.value;
+    let boxProduct="";
+    for(let i=0;i<addProductContainer.length ;i++){
         if( addProductContainer[i].name.toLowerCase().includes(term.toLowerCase())){
         boxProduct +=`
         <tr>
@@ -78,7 +81,7 @@ var term =searchInput.value;
 
 function setData(index){
   indexupdate=index
- var caruntProduct=addProductContainer[index]
+ let caruntProduct=addProductContainer[index]
 ProductNameInput.value=caruntProduct.name;
 ProductPriceInput.value=caruntProduct.price;
 ProductCategoryInput.value=caruntProduct.Category;
@@ -89,11 +92,11 @@ updtateBtn.classList.remove('d-none');
 
 
 function UpdateProduct( ){
-    var  Product = {
-        name: ProductNameInput.value,
-        price: ProductPriceInput.value,
-        Category: ProductCategoryInput.value,
-        Desc: ProductDescInput.value,
+    let  Product = {
+        name: ProductNameInput.getAttribute,
+        price: ProductPriceInput.getAttribute,
+        Category: ProductCategoryInput.getAttribute,
+        Desc: ProductDescInput.getAttribute,
    
     }
    
@@ -106,3 +109,40 @@ updtateBtn.classList.add('d-none');
 }
 
 
+ let massageForUser=document.getElementById("#massageForUser");
+
+function validationName(){
+    let  text=ProductNameInput.value
+    let regex=/^[A-Z][a-z]{3,8}$/;
+if(regex.test(text)==true){
+    ProductNameInput.classList.add("is-valid");
+    ProductNameInput.classList.remove("is-invalid");
+    massageForUser.classList.add("d-none")
+    return true;
+}
+else{
+ProductNameInput.classList.add("is-invalid");
+ProductNameInput.classList.remove("is-valid");
+massageForUser.classList.remove("d-none")
+return false;
+}
+}
+
+let massageForPrice=document.getElementById("#massageForPrice");
+function validationPrice(){
+let test=ProductPriceInput.value;
+let regexPrice=/^[1-10]{5}$/;
+if(regexPrice.test(test)==true){
+    ProductPriceInput.classList.add("is-valid");
+    ProductPriceInput.classList.remove("is-invalid");
+    massageForPrice.classList.add("d-none")
+    return true;
+ 
+}  
+else{
+    ProductPriceInput.classList.add("is-invalid");
+    ProductPriceInput.classList.remove("is-valid");
+    massageForPrice.classList.remove("d-none")
+    return false;
+}  
+}
